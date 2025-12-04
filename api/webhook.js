c90ee1266f338e9f95ae83a65eaee1622eded8f3
nnrsauki <nnrsauki@gmail.com>
2025-12-04 22:11:40 +0100

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,27 +1,19 @@
-const { Client } = require('pg');
+import pg from 'pg';
+const { Client } = pg;
+
 const CONNECTION_STRING = process.env.POSTGRES_URL; 
 const FLW_SECRET_HASH = process.env.FLW_SECRET_HASH; 
 
 export default async function handler(req, res) {
-    const signature = req.headers['verif-hash'];
-    if (!signature || signature !== FLW_SECRET_HASH) {
-        return res.status(401).send('Unverified');
-    }
-
+    if (req.headers['verif-hash'] !== FLW_SECRET_HASH) return res.status(401).send('Unverified');
+    
     const { event, data } = req.body;
-
+    
     if (event === 'charge.completed' && data.status === 'successful') {
-        try {
-            const client = new Client({
-                connectionString: CONNECTION_STRING,
-                ssl: { rejectUnauthorized: false }
-            });
-            await client.connect();
-            console.log(`Webhook logged payment: ${data.id}`);
-            await client.end();
-        } catch (e) {
-            console.error(e);
-        }
+        const client = new Client({ connectionString: CONNECTION_STRING, ssl: { rejectUnauthorized: false } });
+        await client.connect();
+        console.log(`Logged payment: ${data.id}`);
+        await client.end();
     }
     res.status(200).send('OK');
 }
